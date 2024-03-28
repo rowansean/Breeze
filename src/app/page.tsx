ca34e62ab9e94c96651824d2569de5a610f52d0f
Sean-Patrick <rowan.seanp@gmail.com>
2024-03-28 15:18:42 -0400

    feat: started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Card } from '@tremor/react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="b border-4 border-black min-h-screen p-3">
+      <h1>Charlotte</h1>
+      <p>67*</p>
+      <Card className="bg-white">
+        <h1>Hourly Forecast</h1>
+        <ul>
+          <li>12:00 PM: 67*</li>
+          <li>1:00 PM: 67*</li>
+          <li>2:00 PM: 67*</li>
+        </ul>
+      </Card>
     </div>
   );
 }
