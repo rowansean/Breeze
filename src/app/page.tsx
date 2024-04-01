a0e56c6e4ff48dcbb4eb575b3f8b944d25867945
Sean-Patrick <rowan.seanp@gmail.com>
2024-03-31 23:46:33 -0400

    feat: added Header component and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,19 @@ import { SunIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5 border-4 bg-blue-300 border-black min-h-screen p-3 overflow-hidden">
-      <div className="header text-white text-center"></div>
-      <Header>
-        <h1 className="text-4xl drop-shadow-md">Charlotte</h1>
+    <div className="flex flex-col gap-5 bg-gradient-to-b from-[#FFF385] to-[#4182FF] to-70% h-screen w-screen p-3 overflow-x-hidden">
+      <Header className="mt-20">
+        <h1 className="text-4xl drop-shadow-lg">Charlotte</h1>
       </Header>
+      <div className="flex flex-col items-center w-screen">
+        <h1 className="text-8xl text-white drop-shadow-lg my-5">67°</h1>
+        <p className="text-3xl text-white drop-shadow-lg">Sunny</p>
+        <div className="flex gap-3">
+          <p className="text-white drop-shadow-lg">High 70°</p>
+          <p className="text-white drop-shadow-lg">Low 45°</p>
+        </div>
+      </div>
+
       <HourlyForecast />
       <TenDayForecast />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 import { Search, Settings } from "lucide-react";
 import React from "react";
 
-export default function Header({ children }) {
+export default function Header({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="flex justify-between px-5 text-white items-center">
-        <Search />
-        {children}
-        <Settings />
+    <div
+      className={`${className} flex justify-between px-8 text-white items-center`}
+    >
+      <Search size={30} className="drop-shadow-md text-black opacity-45" />
+      {children}
+      <Settings size={30} className="drop-shadow-md text-black opacity-45" />
     </div>
   );
 }
