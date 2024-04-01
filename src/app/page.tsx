8e8ea8f368d2a934ad7a457670041c1a9d10a7ed
Sean-Patrick <rowan.seanp@gmail.com>
2024-04-01 00:27:51 -0400

    feat: added to home page, added styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HourlyForecast from "@/components/HourlyForecast";
 import TenDayForecast from "@/components/TenDayForecast";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Icon } from "@tremor/react";
-import { SunIcon } from "lucide-react";
+import { SunIcon, SunriseIcon, SunsetIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -11,12 +11,25 @@ export default function Home() {
       <Header className="mt-20">
         <h1 className="text-4xl drop-shadow-lg">Charlotte</h1>
       </Header>
-      <div className="flex flex-col items-center w-screen">
-        <h1 className="text-8xl text-white drop-shadow-lg my-5">67°</h1>
-        <p className="text-3xl text-white drop-shadow-lg">Sunny</p>
-        <div className="flex gap-3">
-          <p className="text-white drop-shadow-lg">High 70°</p>
-          <p className="text-white drop-shadow-lg">Low 45°</p>
+
+      <h1 className="text-8xl text-white drop-shadow-lg my-5 text-center ml-8">
+        67°
+      </h1>
+
+      <div className="flex flex-col items-center w-screen font-light mb-5">
+        <div className="flex gap-1">
+          <SunIcon className="drop-shadow-lg text-yellow-300" />
+          <p className=" text-white drop-shadow-lg">Sunny</p>
+        </div>
+
+        <p className="text-white drop-shadow-lg">High 70° | Low 45°</p>
+
+        <div className="flex text-white drop-shadow-lg">
+          <SunriseIcon size={20} />
+          <p>6:30 AM</p>
+          <p className="mx-4">|</p>
+          <SunsetIcon size={20} />
+          <p>8:30 PM</p>
         </div>
       </div>
 
--- src/components/ui/HourlyItem.tsx
+++ src/components/ui/HourlyItem.tsx
@@ -10,24 +10,21 @@ export default function HourlyItem({
   temp: number;
   condition: string;
 }) {
-
-
   const iconMap: { [key: string]: JSX.Element } = {
-    sunny: <SunIcon />,
-    cloudy: <CloudSun />,
-    rainy: <CloudRain />,
-    default: <CircleHelp />,
+    sunny: <SunIcon className="text-yellow-300 drop" />,
+    cloudy: <CloudSun className="text-gray-300 drop" />,
+    rainy: <CloudRain className="text-blue-300 drop" />,
+    default: <CircleHelp className="text-gray-300 drop" />,
     // Add more conditions as needed
   };
 
-  const WeatherIconRender = iconMap[condition] || null;
+  const WeatherIconRender = iconMap[condition] || iconMap.default;
 
   return (
-    <div className=" flex flex-col items-center">
+    <div className="flex flex-col items-center text-sm font-light drop-shadow-lg">
       <h1>{time}</h1>
-      <div>{WeatherIconRender}</div>
-      <p>{condition}</p>
-      <p>{temp}</p>
+      <div className="text-lg m-3">{WeatherIconRender}</div>
+      <p>{temp}°</p>
     </div>
   );
 }
