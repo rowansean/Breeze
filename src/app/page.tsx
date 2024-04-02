ff4d44d8d7bdc4f7b214c12ede9b7c3d9658b547
Sean-Patrick <rowan.seanp@gmail.com>
2024-04-02 15:55:53 -0400

    feat: added data fetch from Tomorrow.io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,24 @@ import Header from "@/components/Header";
 import HourlyForecast from "@/components/HourlyForecast";
 import TenDayForecast from "@/components/TenDayForecast";
 import TodaySummary from "@/components/TodaySummary";
-import AirQualitySmall from "@/components/ui/AirQualitySmall";
 
-export default function Home() {
+async function getData() {
+
+  const location = "toronto"
+
+  fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${location}&apikey=${process.env.API_KEY}`)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+}
+
+export default async function Home() {
+
+  const data = await  getData();
+
+  console.log(data);
+
   return (
     <div className="flex flex-col gap-5 md:max-w-[600px] w-full">
       <Header>
