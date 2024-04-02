197eda64a7b5177955c7a3a0d29a599fed7825f4
Sean-Patrick <rowan.seanp@gmail.com>
2024-04-02 13:55:57 -0400

    fix: Componenet was missing function name

--- src/components/ui/SettingsButton.tsx
+++ src/components/ui/SettingsButton.tsx
@@ -2,7 +2,7 @@ import { Settings } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-export default function () {
+export default function SettingsButton() {
   return (
     <Link href="/">
       <Settings size={30} className="drop-shadow-md text-black opacity-45" />
