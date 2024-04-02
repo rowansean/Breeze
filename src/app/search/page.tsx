fcaf421090f27ac8cb6029bd936566ef7466340d
Sean-Patrick <rowan.seanp@gmail.com>
2024-04-01 23:50:25 -0400

    feat: enhanced search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,18 +1,34 @@
+"use client";
+
+import DynamicSearch from "@/components/DynamicSearch";
 import Header from "@/components/Header";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="flex flex-col gap-5">
       <Header>
-        <h1 className="text-4xl drop-shadow-md">Search</h1>
+        <DynamicSearch />
       </Header>
       <Card>
         <CardHeader>Results...</CardHeader>
         <CardContent>
-          <CardTitle>Search Results</CardTitle>
-          <p>Search results will be displayed here...</p>
+          {searchResults.length ? (
+            searchResults.map((result: any) => (
+              <div key={result.id}>
+                <CardTitle>{result.title}</CardTitle>
+                <p>{result.description}</p>
+              </div>
+            ))
+          ) : (
+            <p className="text-sm font-light text-black opacity-40">
+              No results found...
+            </p>
+          )}
         </CardContent>
       </Card>
     </div>
--- src/components/DynamicSearch.tsx
+++ src/components/DynamicSearch.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Link from "next/link";
 import { Input } from "./ui/input";
+import { useState } from "react";
 
 export default function DynamicSearch() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="flex w-full gap-5 items-center">
       <Input
         className="bg-black bg-opacity-45 rounded-xl"
         placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <Link href="/">
-        <p className="text-black opacity-45 mr-4"> cancel </p>
+        <p className="text-black opacity-45 mr-4">cancel</p>
       </Link>
     </div>
   );
